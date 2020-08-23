fe412e3447ccc343aa7e57a3210f2cf307e6bbec
unknown <kevinqchal@yahoo.com>
2020-08-23 13:55:48 -0700

    first setup for deployment

---
+++ client/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js',
+      publicPath: '/dist/'
+    },
+    devServer: {
+      contentBase: './dist',
+    },
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
+     ]
+    },
+  };
